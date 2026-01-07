bb87974daaac81ece35b52433994b20d8ae40a2e
Er-shuang <1343109389@qq.com>
2026-01-07 21:08:57 +0800

    Create Process Sale interface

---
+++ POSSystem/src/main/java/com/example/possystem/controller/SaleController.java
@@ -0,0 +1,115 @@
+package com.example.possystem.controller;
+
+import com.example.possystem.model.OrderItem;
+import com.example.possystem.model.Product;
+import com.example.possystem.util.SceneSwitcher;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class SaleController {
+
+    @FXML private ListView<Product> productListView;
+    @FXML private TextField quantityField;
+
+    @FXML private TableView<OrderItem> cartTable;
+    @FXML private TableColumn<OrderItem, String> cartNameCol;
+    @FXML private TableColumn<OrderItem, Double> cartPriceCol;
+    @FXML private TableColumn<OrderItem, Integer> cartQtyCol;
+    @FXML private TableColumn<OrderItem, Double> cartSubCol;
+
+    @FXML private Label totalLabel;
+
+    private ObservableList<Product> productList =
+            FXCollections.observableArrayList();
+
+    private ObservableList<OrderItem> cartList =
+            FXCollections.observableArrayList();
+
+    @FXML
+    public void initialize() {
+        productList.addAll(
+                new Product("薯片", 10, 20, "在售", null),
+                new Product("可乐", 8, 5, "在售", null)
+        );
+
+        productListView.setItems(productList);
+
+        cartNameCol.setCellValueFactory(data ->
+                new SimpleStringProperty(data.getValue().getProduct().getName()));
+        cartPriceCol.setCellValueFactory(data ->
+                new SimpleDoubleProperty(data.getValue().getProduct().getPrice()).asObject());
+        cartQtyCol.setCellValueFactory(data ->
+                new SimpleIntegerProperty(data.getValue().getQuantity()).asObject());
+        cartSubCol.setCellValueFactory(data ->
+                new SimpleDoubleProperty(data.getValue().getSubtotal()).asObject());
+
+        cartTable.setItems(cartList);
+    }
+
+    // add to Cashier
+    public void addToCart() {
+
+        Product product = productListView.getSelectionModel().getSelectedItem();
+        if (product == null) return;
+
+        int qty;
+        try {
+            qty = Integer.parseInt(quantityField.getText());
+        } catch (Exception e) {
+            alert("Invalid number format");
+            return;
+        }
+
+        // check out stock
+        if (qty > product.getStock()) {
+            alert("Insufficient stock, current inventory:" + product.getStock());
+            return;
+        }
+
+        // add to Cashier and reduce the stock
+        cartList.add(new OrderItem(product, qty));
+        product.setStock(product.getStock() - qty);
+
+        updateTotal();
+        quantityField.clear();
+    }
+
+    // delete from Cashier
+    public void removeItem() {
+        OrderItem item = cartTable.getSelectionModel().getSelectedItem();
+        if (item == null) return;
+
+        // Deleting will roll back
+        Product p = item.getProduct();
+        p.setStock(p.getStock() + item.getQuantity());
+        cartList.remove(item);
+
+        updateTotal();
+    }
+
+    private void updateTotal() {
+        double total = 0;
+        for (OrderItem item : cartList) {
+            total += item.getSubtotal();
+        }
+        totalLabel.setText(String.valueOf(total));
+    }
+
+    public void submit() {
+        System.out.println("Go to the payment screen (Next)");
+    }
+
+    public void goBack() {
+        SceneSwitcher.switchScene("/com/example/possystem/main.fxml");
+    }
+
+    private void alert(String msg) {
+        Alert a = new Alert(Alert.AlertType.ERROR, msg);
+        a.showAndWait();
+    }
+}
