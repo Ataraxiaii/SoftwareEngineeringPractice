50833e8efdd1f49c0014ebd7319955942317ed41
Ataraxiaii <2722458837@qq.com>
2026-01-08 11:22:55 +0800

    Restore stock when going back to main page

--- POSSystem/src/main/java/com/example/possystem/controller/SaleController.java
+++ POSSystem/src/main/java/com/example/possystem/controller/SaleController.java
@@ -117,7 +117,18 @@ public class SaleController {
     }
 
     public void goBack() {
+        rollbackInventory(); //  restore stock when leaving
         SceneSwitcher.switchScene("/com/example/possystem/main.fxml");
+    }
+
+    public void rollbackInventory() {
+        for (OrderItem item : cartList) {
+            Product p = item.getProduct();
+            // restore cart quantity to memory object
+            p.setStock(p.getStock() + item.getQuantity());
+        }
+        cartList.clear();
+        productListView.refresh();
     }
 
     private void alert(String msg) {
